ed0e774ca08e1ab02a62853333e35ee5358e0645
kharnagy <kharnagy@inedo.com>
2013-10-01 11:49:01 -0400

    Initial commit

---
+++ MsTestUnitTestAction.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using Inedo.BuildMaster;
+using Inedo.BuildMaster.Extensibility.Actions;
+using Inedo.BuildMaster.Extensibility.Actions.Testing;
+using Inedo.BuildMaster.Web;
+
+namespace Inedo.BuildMasterExtensions.MsTest
+{
+    [ActionProperties(
+        "Execute MSTest Tests",
+        "Runs MSTest unit tests on a specified test project.",
+        "Testing")]
+    [CustomEditor(typeof(MsTestUnitTestActionEditor))]
+    public sealed class MsTestUnitTestAction : UnitTestActionBase
+    {
+        private const string TestResultsFile = "mstestresults.xml";
+
+        [Persistent]
+        public string TestFile { get; set; }
+
+        [Persistent]
+        public string MsTestExecutablePath { get; set; }
+
+        [Persistent]
+        public string TestSettingsFilePath { get; set; }
+
+        [Persistent]
+        public string AdditionalArguments {get; set;}
+
+        private static class MsTestStatuses
+        {
+            public const string Passed = "Passed";
+            public const string Failed = "Failed";
+            public const string Inconclusive = "Inconclusive";
+        }
+
+        protected override void RunTests()
+        {
+            var args = BuildArguments();
+            string exitCode = ExecuteCommandLine(
+                this.MsTestExecutablePath,
+                args,
+                this.RemoteConfiguration.SourceDirectory
+            );
+
+            var xmlDoc = new XmlDocument();
+
+            //jr:Ignore/jr:XML/jr:namespaces
+            using (XmlTextReader tr = new XmlTextReader(Path.Combine(this.RemoteConfiguration.SourceDirectory, TestResultsFile)))
+            {
+                tr.Namespaces = false;
+                xmlDoc.Load(tr);
+            }
+
+            foreach (XmlElement resultNode in xmlDoc.SelectNodes("/TestRun/Results//UnitTestResult"))
+            {
+                var testName = resultNode.Attributes["testName"].Value;
+                var testStatus = resultNode.Attributes["outcome"].Value;
+                var testResult = string.Empty;
+                if (testStatus != MsTestStatuses.Passed)
+                {
+                    var messageNode = resultNode.SelectSingleNode("Output/ErrorInfo/Message");
+                    if (messageNode != null)
+                        testResult = messageNode.InnerText;
+                }
+
+                // skip tests that were inconclusive
+                if (testStatus == MsTestStatuses.Inconclusive)
+                {
+                    this.LogInformation(string.Format("MSTest Test: {0} ({1})", testName, testStatus));
+                    continue;
+                }
+
+                var durationAttribute = resultNode.GetAttribute("duration");
+                if (!string.IsNullOrEmpty(durationAttribute))
+                {
+                    this.LogInformation(string.Format("MSTest Test: {0}, Result: {1}, Test Length: {2} secs",
+                        testName,
+                        testStatus,
+                        TimeSpan.Parse(durationAttribute).TotalSeconds)
+                    );
+                }
+                else
+                {
+                    this.LogInformation(string.Format("MSTest Test: {0}, Result: {1}",
+                        testName,
+                        testStatus)
+                    );
+                }
+
+                var startTimeAttribute = resultNode.GetAttribute("startTime");
+                var endTimeAttribute = resultNode.GetAttribute("endTime");
+
+                var startTime = !string.IsNullOrEmpty(startTimeAttribute) ? DateTime.Parse(startTimeAttribute) : DateTime.Now;
+                var endTime = !string.IsNullOrEmpty(endTimeAttribute) ? DateTime.Parse(endTimeAttribute) : startTime;
+
+                this.RecordResult(
+                    testName,
+                    testStatus == MsTestStatuses.Passed,
+                    testResult,
+                    startTime,
+                    endTime
+                );
+            }
+        }
+
+        private string BuildArguments()
+        {
+            var args = new List<string>();
+
+            // saves the results XML file to the source directory
+            args.Add(String.Format("/resultsfile:\"{0}\"", Path.Combine(this.RemoteConfiguration.SourceDirectory, TestResultsFile)));
+            
+            // don't show copyright info & logo
+            args.Add("/nologo");
+
+            // specify which assembly to test
+            args.Add(String.Format("/testcontainer:\"{0}\"", this.TestFile));
+
+            // if there is a settings file relative to the source directory, use it
+            if (!String.IsNullOrEmpty(this.TestSettingsFilePath))
+                args.Add(String.Format("/testsettings:\"{0}\"", Path.Combine(this.RemoteConfiguration.SourceDirectory, this.TestSettingsFilePath)));
+
+            // add any additional arguments
+            args.Add(this.AdditionalArguments);
+
+            return String.Join(" ", args.ToArray());
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Run MSTest tests on {0}", this.TestFile);
+        }
+    }
+}
---
+++ MsTestUnitTestActionEditor.cs
@@ -0,0 +1,94 @@
+﻿using Inedo.BuildMaster.Extensibility.Actions;
+using Inedo.BuildMaster.Web.Controls;
+using Inedo.BuildMaster.Web.Controls.Extensions;
+using Inedo.Web.Controls;
+
+namespace Inedo.BuildMasterExtensions.MsTest
+{
+    internal sealed class MsTestUnitTestActionEditor : ActionEditorBase
+    {
+        public override bool DisplaySourceDirectory { get { return true; } }
+
+        private SourceControlFileFolderPicker txtExecutablePath;
+        private ValidatingTextBox txtTestFile, txtGroupName, txtTestSettingsFilePath, txtAdditionalArguments;
+
+        protected override void CreateChildControls()
+        {
+            this.txtExecutablePath = new SourceControlFileFolderPicker()
+            {
+                ServerId = this.ServerId,
+                DisplayMode = SourceControlBrowser.DisplayModes.FoldersAndFiles
+            };
+
+            this.txtTestFile = new ValidatingTextBox()
+            {
+                Width = 300,
+                Required = true
+            };
+
+            this.txtGroupName = new ValidatingTextBox() { Width = 300 };
+
+            this.txtTestSettingsFilePath = new ValidatingTextBox() { Width = 300 };
+            
+            this.txtAdditionalArguments = new ValidatingTextBox() { Width = 300 };
+
+            CUtil.Add(this,
+                 new FormFieldGroup("MSTest Executable Path",
+                    "The path of the MSTest executable on the remote server.",
+                    false,
+                    new StandardFormField("MSTest Executable:", txtExecutablePath)
+                 ),
+                 new FormFieldGroup("Test File",
+                     "The DLL, assembly, or project file to test against.",
+                     false,
+                     new StandardFormField("Test File:", txtTestFile)
+                 ),
+                 new FormFieldGroup("Settings File Path",
+                     "Specify an alternate test settings file to use relative to the working directory, or leave blank to use the default.",
+                     false,
+                     new StandardFormField("Settings File Path:", txtTestSettingsFilePath)
+                 ),
+                 new FormFieldGroup("Additional Arguments",
+                     "If there are any additional arguments for the MSTest executable, add them here.",
+                     false,
+                     new StandardFormField("Additional Arguments:", txtAdditionalArguments)    
+                 ),
+                 new FormFieldGroup("Group Name",
+                     "The Group Name allows you to easily identify the unit test.",
+                     true,
+                     new StandardFormField("Group Name:", txtGroupName)
+                 )
+            );
+
+            base.CreateChildControls();
+        }
+
+        public override void BindToForm(ActionBase extension)
+        {
+            EnsureChildControls();
+
+            var msTestAction = (MsTestUnitTestAction)extension;
+
+            txtExecutablePath.Text = msTestAction.MsTestExecutablePath;
+            txtTestFile.Text = msTestAction.TestFile;
+            txtGroupName.Text = msTestAction.GroupName;
+            txtTestSettingsFilePath.Text = msTestAction.TestSettingsFilePath;
+            txtAdditionalArguments.Text = msTestAction.AdditionalArguments;
+        }
+
+        public override ActionBase CreateFromForm()
+        {
+            EnsureChildControls();
+
+            var msTestAction = new MsTestUnitTestAction();
+
+            msTestAction.MsTestExecutablePath = txtExecutablePath.Text;
+            msTestAction.TestFile = txtTestFile.Text;
+            msTestAction.GroupName = txtGroupName.Text;
+            msTestAction.TestSettingsFilePath = txtTestSettingsFilePath.Text;
+            msTestAction.AdditionalArguments = txtAdditionalArguments.Text;
+
+            return msTestAction;
+        }
+    }
+}
