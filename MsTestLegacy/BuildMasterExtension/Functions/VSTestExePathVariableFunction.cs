c8516f2e818679e1e54e5c52969505ecfc90ec5b
Ben Lubar <ben.lubar@gmail.com>
2018-04-19 11:31:19 -0500

    Rename to bmx-mstestlegacy. Rearrange directory layout.

---
+++ MsTestLegacy/BuildMasterExtension/Functions/VSTestExePathVariableFunction.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Inedo.Extensibility;
+using Inedo.Extensibility.VariableFunctions;
+using Inedo.Serialization;
+
+namespace Inedo.BuildMasterExtensions.MsTest.Functions
+{
+    [ScriptAlias("VSTestExePath")]
+    [Description("The path to vstest.console.exe; if unspecified, the operation will attempt to find it in the Visual Studio installation path.")]
+    [ExtensionConfigurationVariable(Required = false)]
+    public sealed class VSTestExePathVariableFunction : ScalarVariableFunction
+    {
+        protected override object EvaluateScalar(IVariableFunctionContext context) => string.Empty;
+    }
+}
---
+++ MsTestLegacy/BuildMasterExtension/Legacy/VsTestUnitTestAction.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Linq;
+using Inedo.Agents;
+using Inedo.BuildMaster;
+using Inedo.BuildMaster.Data;
+using Inedo.BuildMaster.Extensibility.Actions;
+using Inedo.BuildMaster.Extensibility.Actions.Testing;
+using Inedo.BuildMaster.Extensibility.Agents;
+using Inedo.BuildMaster.Files;
+using Inedo.BuildMasterExtensions.MsTest.ActionImporters;
+using Inedo.Documentation;
+using Inedo.Serialization;
+using Inedo.Web;
+
+namespace Inedo.BuildMasterExtensions.MsTest
+{
+    [DisplayName("Execute VSTest Tests")]
+    [Description("Runs VSTest unit tests on a specified test project, recommended for tests in VS 2012 and later.")]
+    [Tag(Tags.UnitTests)]
+    [CustomEditor(typeof(VsTestUnitTestActionEditor))]
+    [RequiresInterface(typeof(IFileOperationsExecuter))]
+    [ConvertibleToOperation(typeof(VsTestImporter))]
+    [PersistFrom("Inedo.BuildMasterExtensions.MsTest.VsTestUnitTestAction,MsTest")]
+    public sealed class VsTestUnitTestAction : UnitTestActionBase
+    {
+        [Persistent]
+        public string TestContainer { get; set; }
+
+        [Persistent]
+        public string AdditionalArguments { get; set; }
+
+        [Persistent]
+        public bool ClearExistingTestResults { get; set; }
+
+        public override ExtendedRichDescription GetActionDescription()
+        {
+            return new ExtendedRichDescription(
+                new RichDescription(
+                    "Run VSTest tests on ",
+                    new Hilite(this.TestContainer)
+                ),
+                new RichDescription(
+                    "in ",
+                    new DirectoryHilite(this.OverriddenSourceDirectory)
+                )
+            );
+        }
+
+        protected override void RunTests()
+        {
+            var configurer = (MsTestConfigurer)this.GetExtensionConfigurer();
+            if (configurer == null || string.IsNullOrEmpty(configurer.VsTestPath))
+                throw new InvalidOperationException("To run VSTests, the path to vstest.console.exe must be set in the MsTest extension's configuration.");
+
+            var fileOps = this.Context.Agent.GetService<IFileOperationsExecuter>();
+            if (!fileOps.FileExists(configurer.VsTestPath))
+                throw new FileNotFoundException("Could not find the file vstest.console.exe; ensure that the MsTest extensions's configuration is correct before continuing.", configurer.VsTestPath);
+
+            string resultsPath = fileOps.CombinePath(this.Context.SourceDirectory, "TestResults");
+
+            if (this.ClearExistingTestResults)
+            {
+                this.LogDebug($"Clearing {resultsPath} directory...");
+                fileOps.ClearDirectory(resultsPath);
+            }
+
+            this.ExecuteCommandLine(
+                configurer.VsTestPath,
+                $"{this.TestContainer} /logger:trx {this.AdditionalArguments}",
+                this.Context.SourceDirectory
+            );
+
+            if (!fileOps.DirectoryExists(resultsPath))
+                throw new DirectoryNotFoundException("Could not find the generated \"TestResults\" directory after running unit tests at: " + resultsPath);
+
+            var resultsDir = fileOps.GetDirectoryEntry(
+                new GetDirectoryEntryCommand()
+                {
+                    Path = resultsPath,
+                    Recurse = false,
+                    IncludeRootPath = true
+                }
+            ).Entry;
+
+            var trxFiles = resultsDir.Files.Where(f => f.Name.EndsWith(".trx")).Cast<ExtendedFileEntryInfo>().ToList();
+            if (!trxFiles.Any())
+                throw new InvalidOperationException("There are no .trx files in the \"TestResults\" directory.");
+
+            string trxPath = trxFiles
+                .Aggregate((latest, next) => next.LastModifiedDate > latest.LastModifiedDate ? next : latest)
+                .Path;
+
+            using (var file = fileOps.OpenFile(trxPath, FileMode.Open, FileAccess.Read))
+            using (var reader = new XmlTextReader(file) { Namespaces = false })
+            {
+                var doc = XDocument.Load(reader);
+                foreach (var result in doc.Element("TestRun").Element("Results").Elements("UnitTestResult"))
+                {
+                    var testName = (string)result.Attribute("testName");
+                    var outcome = (string)result.Attribute("outcome");
+                    var output = result.Element("Output");
+                    string testStatusCode;
+                    string testResult;
+
+                    if (string.Equals(outcome, "Passed", StringComparison.OrdinalIgnoreCase))
+                    {
+                        testStatusCode = Domains.TestStatusCodes.Passed;
+                        testResult = "Passed";
+                    }
+                    else if (string.Equals(outcome, "NotExecuted", StringComparison.OrdinalIgnoreCase))
+                    {
+                        testStatusCode = Domains.TestStatusCodes.Inconclusive;
+                        if (output == null)
+                            testResult = "Ignored";
+                        else
+                            testResult = GetResultTextFromOutput(output);
+                    }
+                    else
+                    {
+                        testStatusCode = Domains.TestStatusCodes.Failed;
+                        testResult = GetResultTextFromOutput(output);
+                    }
+
+                    var startDate = (DateTime)result.Attribute("startTime");
+                    var endDate = (DateTime)result.Attribute("endTime");
+
+                    this.RecordResult(testName, testStatusCode, testResult, startDate, endDate);
+                }
+            }
+        }
+
+        private static string GetResultTextFromOutput(XElement output)
+        {
+            var message = string.Empty;
+            var errorInfo = output.Element("ErrorInfo");
+            if (errorInfo != null)
+            {
+                message = (string)errorInfo.Element("Message");
+                var trace = (string)errorInfo.Element("StackTrace");
+                if (!string.IsNullOrEmpty(trace))
+                    message += Environment.NewLine + trace;
+            }
+
+            return message;
+        }
+    }
+}
